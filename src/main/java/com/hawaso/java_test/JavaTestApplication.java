0c3c961bd139d52aebcd2407513a6a1555a9c176
jenkins <yjhan713@gmail.com>
2023-11-20 15:00:54 +0900

    테스트

---
+++ src/main/java/com/hawaso/java_test/JavaTestApplication.java
@@ -0,0 +1,14 @@
+package com.hawaso.java_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaTestApplication.class, args);
+	}
+
+}
